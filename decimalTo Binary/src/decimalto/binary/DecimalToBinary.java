cf654617e084255b5ecba8f0cb51a2a8b17a6361
Victoria Nyamai <61185176+Tori-Stark@users.noreply.github.com>
2021-04-06 20:09:57 +0300

    Update DecimalToBinary.java
    The user can now calculate for their own values

--- decimalTo Binary/src/decimalto/binary/DecimalToBinary.java
+++ decimalTo Binary/src/decimalto/binary/DecimalToBinary.java
@@ -6,6 +6,8 @@ import java.text.DecimalFormat;
 import java.util.*;
 import java.util.Random;
 
+import java.util.Scanner;
+
 
 public class DecimalToBinary {
        
@@ -84,40 +86,92 @@ public class DecimalToBinary {
   
     // Driver code
     public static void main(String[] args) {
+        System.out.println("Hello. Welcome to Wale Wase Float Converters.\n");
         
-        System.out.println("S/No.\t\tDecimal Number\t\tBinary Number\t\tRemarks");
+        
+        System.out.println(" \nIf you would like to output 30 random numbers,press 1 \nIf you would like to input the numbers to convert, press 2");
         
         
-        for(int j=1;j<=30;j++){
-            DecimalFormat df2 = new DecimalFormat(".###");
-            double min = 0;
-            double max = 100.135;
-            double diff = max - min;
-            double randomValue = min + Math.random( ) * diff;
-              DecimalFormat df3 = new DecimalFormat(".#####");
-            BigDecimal bd=new BigDecimal(decimalToBinary(randomValue));
-            int binaryNumber = Integer.parseInt(df3.format(bd));
-            
-            String doubleNumber = decimalToBinary(binaryNumber);
-            String doubleAsString = String.valueOf(doubleNumber);
-            int indexOfDecimal = doubleAsString.indexOf(".");
-            String str = String.valueOf(doubleAsString.substring(indexOfDecimal));
-            int charCount = str.length()-1;
-            
-        if(charCount>5){
-          
-           
-           
-            System.out.printf(j+"\t\t"+df2.format(randomValue)+"\t\t"+df3.format(bd)+"\t\tApproximate\n", j,j,j,j,j);
-        }
-        else
-        {
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+            switch (number) {
+                case 1:
+                    System.out.println("S/No.\t\tDecimal Number\t\tBinary Number\t\tRemarks");
+                    for(int j=1;j<=30;j++){
+                        
+                        
+                        DecimalFormat df2 = new DecimalFormat(".###");
+                        double min = 0;
+                        double max = 10.135;
+                        double diff = max - min;
+                        double randomValue = min + Math.random( ) * diff;
+                        DecimalFormat df3 = new DecimalFormat(".#####");
+                        BigDecimal bd=new BigDecimal(decimalToBinary(randomValue));
+                        
+                        double binaryNumber=Double.parseDouble(df2.format(randomValue));
+//            doubleAsString.substring(indexOfDecimal)
 
-           
-            System.out.printf(j+"\t\t"+df2.format(randomValue)+"\t\t"+df3.format(bd)+"\t\tExact\n", j,j,j,j,j);  
+                        String doubleNumber = df3.format(bd);
+
+                        String doubleAsString = String.valueOf(doubleNumber);
+                        int indexOfDecimal = doubleAsString.indexOf(".");
+                        String str = String.valueOf(doubleAsString.substring(indexOfDecimal));
+                        int charCount = str.length()-1;
+
+                        if(charCount>4){
+
+
+
+                            System.out.printf(j+"\t\t"+df2.format(randomValue)+"\t\t"+df3.format(bd)+"\t\tApproximate\n", j,j,j,j,j);
+                        }
+                        else
+                        {
+
+
+                            System.out.printf(j+"\t\t"+df2.format(randomValue)+"\t\t"+df3.format(bd)+"\t\tExact\n", j,j,j,j,j);
+
+}}          break;
+                case 2:
+                    System.out.println(" Input the value you would like to convert");
+                    Scanner input2 = new Scanner(System.in);
+                    float value = input2.nextFloat();
+                     
+                       
+                        DecimalFormat df3 = new DecimalFormat(".#####");
+                        BigDecimal bd=new BigDecimal(decimalToBinary(value));
+                        
+                        
+
+
+                        String doubleNumber = decimalToBinary(value);
+
+                        String doubleAsString = String.valueOf(doubleNumber);
+                        int indexOfDecimal = doubleAsString.indexOf(".");
+                        String str = String.valueOf(doubleAsString.substring(indexOfDecimal));
+                        int charCount = str.length()-1;
+                        System.out.println("S/No.\t\tDecimal Number\t\tBinary Number\t\tRemarks");
+                    if(charCount>5){
+                        
+                        
+                        
+                        System.out.printf(1+"\t\t"+value+"\t\t"+df3.format(decimalToBinary(value))+"\t\tApproximate\n");
+                    }
+                    else
+                    {
+                        
+                        
+                        System.out.printf(1+"\t\t"+value+"\t\t"+decimalToBinary(value)+"\t\tExact\n");
+                        
+                    }           break;
+                default:
+                    System.out.printf("Wrong Choice! Try Again");
+                    break;
+            }
+            System.out.printf("Thank you!");
         
-        }
         
+        
+              
         
 
 
@@ -129,4 +183,4 @@ public class DecimalToBinary {
     }
     
     }
-}
+
